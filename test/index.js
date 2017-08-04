9cc00aab11223818ddd7439dddc63f6f12d5df23
Tony Kung <tony.kung@bitgamelabs.com>
2017-08-04 15:18:11 +0800

    ability to parse yaml, look up files starting from metalsmith source folder, output will be objects keyed with relative file path without file suffix

--- test/index.js
+++ test/index.js
@@ -5,9 +5,9 @@ var Metalsmith = require('metalsmith')
 var metadata = require('../lib/index.js')
 
 it('should read a single JSON file and put into metalsmith.metadata()', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-single').use(metadata({ directory: 'src/**/*.json' }))
+  var metalsmith = Metalsmith('test/fixtures/json-single').source('./src').use(metadata({ directory: 'src/**/*.json' }))
   metalsmith.build(function(err) {
-    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' } })
+    metalsmith.metadata().should.deep.equal({ 'src/example': { text: 'Text from a json file' } })
 
     if (err) {
       return done(err)
@@ -18,9 +18,9 @@ it('should read a single JSON file and put into metalsmith.metadata()', function
 })
 
 it('should read multiple JSON files and put into metalsmith.metadata()', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-multiple').use(metadata({ directory: 'test/fixtures/json-multiple/src/**/*.json' }))
+  var metalsmith = Metalsmith('test/fixtures/json-multiple').source('./src').use(metadata({ directory: 'test/fixtures/json-multiple/src/**/*.json' }))
   metalsmith.build(function(err) {
-    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' }, site: { url: 'http://test.dev' } })
+    metalsmith.metadata().should.deep.equal({ 'src/example': { text: 'Text from a json file' }, 'src/subdirectory/site': { url: 'http://test.dev' } })
 
     if (err) {
       return done(err)
@@ -31,9 +31,9 @@ it('should read multiple JSON files and put into metalsmith.metadata()', functio
 })
 
 it('should read multiple JSON files, including subdirctories and put into metalsmith.metadata()', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-subdirectory').use(metadata({ directory: 'test/fixtures/json-subdirectory/src/**/*.json' }))
+  var metalsmith = Metalsmith('test/fixtures/json-subdirectory').source('./src').use(metadata({ directory: 'test/fixtures/json-subdirectory/src/**/*.json' }))
   metalsmith.build(function(err) {
-    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' }, site: { url: 'http://test.dev' } })
+    metalsmith.metadata().should.deep.equal({ 'src/example': { text: 'Text from a json file' }, 'src/subdirectory/site': { url: 'http://test.dev' } })
 
     if (err) {
       return done(err)
@@ -44,31 +44,31 @@ it('should read multiple JSON files, including subdirctories and put into metals
 })
 
 it('should error if a JSON file is malformed', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-malformed').use(metadata({ directory: 'test/fixtures/json-malformed/src/**/*.json' }))
+  var metalsmith = Metalsmith('test/fixtures/json-malformed').source('./src').use(metadata({ directory: 'test/fixtures/json-malformed/src/**/*.json' }))
   metalsmith.build(function(err) {
     errMessage = String(err);
     err.should.be.an('error')
-    errMessage.should.equal('Error: Malformed data in: example.json')
+    errMessage.should.equal('Error: Malformed .json data in example.json')
 
     done() // don't return the error to metalsmith
   })
 })
 
-it('should error if a key is already used', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-duplicate').use(metadata({ directory: 'test/fixtures/json-duplicate/src/**/*.json' }))
-  metalsmith.build(function(err) {
-    errMessage = String(err);
-    err.should.be.an('error')
-    errMessage.should.equal('Error: Duplicate file name: example.json')
+// it('should error if a key is already used', function (done) {
+//   var metalsmith = Metalsmith('test/fixtures/json-duplicate').source('./src').use(metadata({ directory: 'test/fixtures/json-duplicate/src/**/*.json' }))
+//   metalsmith.build(function(err) {
+//     errMessage = String(err);
+//     err.should.be.an('error')
+//     errMessage.should.equal('Error: Duplicate file name: example.json')
 
-    done() // don't return the error to metalsmith
-  })
-})
+//     done() // don't return the error to metalsmith
+//   })
+// })
 
 it('should ignore a JSON file if is empty', function (done) {
-  var metalsmith = Metalsmith('test/fixtures/json-empty').use(metadata({ directory: 'test/fixtures/json-empty/src/**/*.json' }))
+  var metalsmith = Metalsmith('test/fixtures/json-empty').source('./src').use(metadata({ directory: 'test/fixtures/json-empty/src/**/*.json' }))
   metalsmith.build(function(err) {
-    metalsmith.metadata().should.deep.equal({ example: { text: 'Text from a json file' } })
+    metalsmith.metadata().should.deep.equal({ 'src/example': { text: 'Text from a json file' } })
 
     if (err) {
       return done(err)
